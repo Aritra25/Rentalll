a7d3b06aebf3541f5089146970eae2888e8ab97b
Aritra25 <aritra.maji2001@gmail.com>
2025-05-01 18:18:23 +0530

    second commit

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,12 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "example.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.amazonaws.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
