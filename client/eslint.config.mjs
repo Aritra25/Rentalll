4013d1c224412ba9848801a845e71c06476eb777
Aritra25 <aritra.maji2001@gmail.com>
2025-05-01 17:33:19 +0530

    first commit4

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -16,7 +16,7 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
 ];
